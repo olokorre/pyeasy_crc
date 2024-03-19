43a065ca0f041330e702b2c251eaf0540b009122
Andrei Ruan Steffens <andrei@movitech.com.br>
2024-03-19 16:20:29 -0300

    implementado preimeiro algoritimo

---
+++ source/algorithms/Algorithms.py
@@ -0,0 +1,7 @@
+from source.algorithms.Algorithm import Algorithm
+from source.algorithms.i_code import I_CODE
+from source.types.AlgorithmKeys import AlgorithmKeys
+
+algorithms: dict[AlgorithmKeys, Algorithm] = {
+   'I-CODE': I_CODE
+}
---
+++ test.py
@@ -0,0 +1,5 @@
+from source.crc8 import crc8
+
+
+code = crc8('I-CODE', 'Olá, mundo')
+print(code)
