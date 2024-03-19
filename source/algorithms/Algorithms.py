67de91ff9403b54df0e562c5dfff473463a1cc17
Andrei Ruan Steffens <andrei@movitech.com.br>
2024-03-19 17:07:04 -0300

    Implementado alguritimos que serão ultilizados

--- source/algorithms/Algorithms.py
+++ source/algorithms/Algorithms.py
@@ -1,7 +1,17 @@
 from source.algorithms.Algorithm import Algorithm
+from source.algorithms.cdma2000 import CDMA2000
+from source.algorithms.ebu import EBU
 from source.algorithms.i_code import I_CODE
+from source.algorithms.itu import ITU
+from source.algorithms.maxim import MAXIM
+from source.algorithms.rohc import ROHC
 from source.types.AlgorithmKeys import AlgorithmKeys
 
 algorithms: dict[AlgorithmKeys, Algorithm] = {
-   'I-CODE': I_CODE
+   'I-CODE': I_CODE,
+   'ROHC': ROHC,
+   'CDMA2000': CDMA2000,
+   'EBU': EBU,
+   'ITU': ITU,
+   'MAXIM': MAXIM
 }
--- test.py
+++
@@ -1,5 +0,0 @@
-from source.crc8 import crc8
-
-
-code = crc8('I-CODE', 'Olá, mundo')
-print(code)
